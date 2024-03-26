2d278146e81fb635e8d805d2e40d9b5f71bafc73
PENTALOG\cnguyenhoang <cnguyenhoang@pentalog.com>
2024-03-26 16:50:22 +0700

    First commit

---
+++ TheFirstDay/TheFirstDay.py
@@ -0,0 +1,7 @@
+#print whatever do you want:
+
+print("Hellow world")
+print('hellow world - what is difference between \"\" and \'\' ')
+
+name = "first day with python"
+print(name.find("python"))
